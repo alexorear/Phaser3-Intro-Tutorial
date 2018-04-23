6077677672d59827bf840e5d7d101b41a6cbcb0b
alexorear <alex.orear@gmail.com>
2018-04-22 20:36:01 -0500

    Reduce physics gravity. Fix character import asset reference. Create character and platforms.

--- dist/bundle.js
+++ dist/bundle.js
@@ -10992,14 +10992,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 let config = {
 	type: Phaser.AUTO,
-	parent: 'content',
 	width: 800,
 	height: 600,
-	scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 500 },
+			gravity: { y: 300 },
 			debug: false
 		}
 	},
@@ -11018,12 +11016,43 @@ function preload() {
 	this.load.image('ground', 'assets/platform.png');
 	this.load.image('star', 'assets/star.png');
 	this.load.image('bomb', 'assets/bomb.png');
-	this.load.spritesheet('dude', 'assets/sky.png', { frameWidth: 32, frameHeight: 48 });
+	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
 	this.add.image(0, 0, 'sky').setOrigin(0, 0);
-	this.add.image(400, 300, 'star');
+
+	// level platforms
+	let platforms = this.physics.add.staticGroup();
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(600, 400, 'ground');
+	platforms.create(50, 250, 'ground');
+	platforms.create(750, 220, 'ground');
+
+	// player character
+	let player = this.physics.add.sprite(100, 450, 'dude');
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+
+	this.anims.create({
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	this.anims.create({
+		key: 'turn',
+		frames: [{ key: 'dude', frame: 4 }],
+		frameRate: 20
+	});
+
+	this.anims.create({
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+		frameRate: 10,
+		repeat: -1
+	});
 }
 
 function update() {}
--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import 'phaser';
 
 let config = {
 	type: Phaser.AUTO,
-	parent: 'content',
 	width: 800,
 	height: 600,
-	scaleMode: 0, //Phaser.ScaleManager.EXACT_FIT,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 500 },
+			gravity: { y: 300 },
 			debug: false
 		}
 	},
@@ -28,14 +26,46 @@ function preload() {
 	this.load.image('ground', 'assets/platform.png');
 	this.load.image('star', 'assets/star.png');
 	this.load.image('bomb', 'assets/bomb.png');
-	this.load.spritesheet('dude', 'assets/sky.png',
+	this.load.spritesheet('dude', 'assets/dude.png',
 		{ frameWidth: 32, frameHeight: 48 }
 	);
 }
 
 function create() {
 	this.add.image(0, 0, 'sky').setOrigin(0, 0);
-	this.add.image(400, 300, 'star');
+
+	// level platforms
+	let platforms = this.physics.add.staticGroup();
+	platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+	platforms.create(600, 400, 'ground');
+	platforms.create(50, 250, 'ground');
+	platforms.create(750, 220, 'ground');
+
+	// player character
+	let player = this.physics.add.sprite(100, 450, 'dude');
+	player.setBounce(0.2);
+	player.setCollideWorldBounds(true);
+
+	this.anims.create({
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
+	this.anims.create({
+		key: 'turn',
+		frames: [{ key: 'dude', frame: 4 }],
+		frameRate: 20
+	});
+
+	this.anims.create({
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+		frameRate: 10,
+		repeat: -1
+	});
+
 }
 
 function update() {
